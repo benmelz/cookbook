09582638383986f80a78af08be1a658ec148e87a
benmelz <ben@melz.me>
2024-03-02 14:45:25 -0500

    add linters

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+import '@hotwired/turbo-rails';
 import * as Popper from '@popperjs/core';
 import 'bootstrap';
-import "./controllers"
+import './controllers';
